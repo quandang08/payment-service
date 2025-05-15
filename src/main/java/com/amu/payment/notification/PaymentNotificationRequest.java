3cedef07a4b4c1d5b7c7d9707a108f29200aa148
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-05-15 12:24:06 +0700

    feat(payment): implement PaymentNotificationRequest and Kafka producer to publish payment notification events.
    - Added `PaymentNotificationRequest` record to define the structure of payment notification message
    - Implemented `NotificationProducer` service to publish Kafka messages to `payment-topic`
    - Integrated producer logic in `PaymentService` to send notification event after successful payment creation

---
+++ src/main/java/com/amu/payment/notification/PaymentNotificationRequest.java
@@ -0,0 +1,15 @@
+package com.amu.payment.notification;
+
+import com.amu.payment.entities.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstName,
+        String customerLastName,
+        String customerEmail
+) {
+}
--- src/main/java/com/amu/payment/service/PaymentService.java
+++ src/main/java/com/amu/payment/service/PaymentService.java
@@ -2,8 +2,9 @@ package com.amu.payment.service;
 
 import com.amu.payment.dto.request.PaymentRequest;
 import com.amu.payment.mapper.PaymentMapper;
+import com.amu.payment.notification.NotificationProducer;
+import com.amu.payment.notification.PaymentNotificationRequest;
 import com.amu.payment.repositories.PaymentRepository;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +14,21 @@ public class PaymentService {
 
     private final PaymentRepository repository;
     private final PaymentMapper mapper;
+    private final NotificationProducer notificationProducer;
 
-    public Integer createPayment(@Valid PaymentRequest request) {
+    public Integer createPayment(PaymentRequest request) {
         var payment = repository.save(mapper.toPayment(request));
-        return null;
+
+        notificationProducer.sendNotification(
+                new PaymentNotificationRequest(
+                        request.orderReference(),
+                        request.amount(),
+                        request.paymentMethod(),
+                        request.customer().firstName(),
+                        request.customer().lastName(),
+                        request.customer().email()
+                )
+        );
+        return payment.getId();
     }
 }
